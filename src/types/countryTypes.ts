6a2928c74a11949e731293c66535b9d74cb7b7f4
Captain-Kim <brighthero@kakao.com>
2024-06-26 02:27:31 +0900

    add: API 응답값 타입 지정

---
+++ src/types/countryTypes.ts
@@ -0,0 +1,11 @@
+export type CountryData = {
+    name: {
+        common: string;
+        official: string;
+    };
+    cca3: string;
+    capital: string[];
+    flags: {
+        png: string;
+    };
+};
