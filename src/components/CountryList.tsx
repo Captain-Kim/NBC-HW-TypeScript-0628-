d27ac4916f4b1b21c46fe184a7348dec688d91eb
Captain-Kim <brighthero@kakao.com>
2024-06-26 12:17:46 +0900

    add: List 컴포넌트 기본 셋업

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,6 +1,24 @@
+import styled from 'styled-components';
+import CountryCard from './CountryCard';
+
+const CardSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
 const CountryList = () => {
-    return(
+
+    return (
         <>
+            <h1>내가 고른 카드</h1>
+            <CardSection>
+                <CountryCard />
+            </CardSection>
+            <h1>국가 목록</h1>
+            <CardSection>
+                <CountryCard />
+            </CardSection>
         </>
     )
 }
