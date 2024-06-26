e72c8f4285989a9b45f579f7046627ff60897d6d
Captain-Kim <brighthero@kakao.com>
2024-06-26 22:06:27 +0900

    add: Card 컴포넌트 props 전달 받도록 설정

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,12 @@
-const CountryCard = () => {
+import { CountryData } from '../types/countryTypes';
+
+interface CountryCardProps {
+    country: CountryData;
+    onCountryClick?: (country: CountryData) => void;
+    selected?: boolean;
+  }
+
+const CountryCard: React.FC<CountryCardProps> = ({ country, onCountryClick, selected }) => {
     return(
         <>
         </>
