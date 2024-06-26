a0e24aed302392027910026aefa6e7fddbe032ee
Captain-Kim <brighthero@kakao.com>
2024-06-26 22:51:52 +0900

    add: 카드 렌더링

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,4 +1,37 @@
 import { CountryData } from '../types/countryTypes';
+import styled from 'styled-components';
+
+const StyledCard = styled.div<{ selected?: boolean }>`
+  border: 1px solid black;
+  border-radius: 20px;
+  width: 250px;
+  height: 150px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
+
+const StyledFlagImg = styled.div`
+  width: 100px;
+  height: 50px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const StyledTitle = styled.div`
+  font-size: 25px;
+  font-weight: 800;
+  margin-top: 10px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+`;
 
 type CountryCardProps = {
     country: CountryData;
@@ -7,10 +40,17 @@ type CountryCardProps = {
 }
 
 const CountryCard: React.FC<CountryCardProps> = ({ country, onCountryClick, selected }) => {
+
     return (
-        <>
-        </>
-    )
+        <StyledCard>
+            <StyledFlagImg>
+                <img src={country.flags.png} alt={`국기 이미지`} />
+            </StyledFlagImg>
+            <StyledTitle>{country.name.common}</StyledTitle>
+            <p>{country.capital}</p>
+        </StyledCard>
+    );
+
 }
 
 export default CountryCard;
